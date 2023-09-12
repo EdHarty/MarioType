05f044690062e30b325a2cb4a7cdb1f952980773
CarlGauss1 <eharty1@outlook.com>
2023-09-12 14:14:48 +0000

    Added plater movement

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const gravity = 0.5
+const gravity = 1.5
 class Player {
     constructor() {
         this.position = {
@@ -26,9 +26,9 @@ class Player {
     }
 
     update() {
-        this.draw()
+        this.draw() 
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y 
-        
         if (this.position.y +this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity
         else this.velocity.y = 0
@@ -36,12 +36,79 @@ class Player {
 }
 
 const player = new Player()
-
+const keys = {
+    right:{
+        pressed: false
+    },
+    left:{
+        pressed: false
+    },
+}
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
 
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
+
 }
 
 animate()
+
+document.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    switch (event.key){
+        case 'a' :
+            keys.left.pressed = true
+            break
+        case 's':
+            break
+        case 'd':
+            keys.right.pressed = true
+            break
+        case 'w':
+            player.velocity.y -= 20
+            break
+        /*case 'ArrowLeft' :
+            break
+        case 'ArrowDown':
+            break
+        case 'ArrowRight':
+            player.velocity.x = 1
+            break
+        case 'ArrowUp':
+            player.velocity.y -= 20
+            break*/
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    console.log(event.key)
+    switch (event.key){
+        case 'a' :
+            keys.left.pressed = false
+            break
+        case 's':
+            break
+        case 'd':
+            keys.right.pressed = false
+            break
+        case 'w':
+            player.velocity.y -= 20
+            break
+       /* case 'ArrowLeft' :
+            break
+        case 'ArrowDown':
+            break
+        case 'ArrowRight':
+            player.velocity.x = 0
+            break
+        case 'ArrowUp':
+            player.velocity.y -= 20
+            break*/
+    }
+})
